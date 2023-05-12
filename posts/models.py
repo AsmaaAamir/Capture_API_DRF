791560e2d81b06dc5a2c6daaf275742c65b6e5cf
AsmaaAamir <>
2023-05-12 18:28:38 +0000

    Added post models in posts app and did the migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
+# Imports
+# -------------------------------------------------
+# 3rd Parties:-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# -------------------------------------------------
+
+
+class Post(models.Model):
+    """
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='images/', default='https://res.cloudinary.com/doow4kmj4/image/upload/v1683901095/capture_logo_nbnr9p.jpg',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
