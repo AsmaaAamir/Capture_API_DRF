7e0d00029388eeb47d0bdf90ff4672ec9e101bab
AsmaaAamir <>
2023-05-12 17:16:24 +0000

    Added profile details and tested is it can changed

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 # Imports
 # -------------------------------------------------
 # 3rd Parties:-
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 
@@ -12,9 +14,37 @@ from .serializers import ProfileSerializer
 
 class ProfileList(APIView):
     """
-    Created a class view for the ProfileList
+    Created a class to view all Profile's in a list
     """
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+
+
+class ProfileDetail(APIView):
+    """
+    Created a class to edit profile's 
+    """
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
