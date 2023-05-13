91b0a3490689ab6d6c5ae74aefb21992cea4726d
AsmaaAamir <>
2023-05-13 16:11:29 +0000

    Auth user are able to edit and deleted their posts and added errors if the post doesn't exists

--- posts/urls.py
+++ posts/urls.py
@@ -3,12 +3,12 @@
 # 3rd Parties:-
 from django.urls import path
 
-# internal: path('posts/<int:pk>/', views.PostDetail.as_view()),
+# internal:,
 from posts import views
 # -------------------------------------------------
 
 
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
-   
+    path('posts/<int:pk>/', views.PostDetail.as_view())
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 # Imports
 # -------------------------------------------------
 # 3rd Parties:-
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response 
@@ -8,22 +9,27 @@ from rest_framework.response import Response
 # internal:
 from .models import Post
 from .serializers import PostSerializer
+from capture.permissions import IsOwnerOrReadOnly
 # -------------------------------------------------
 
 
 class PostList(APIView):
+    """
+    In post list, you will be abel to view all posts in a list 
+    and add posts to. Also added permissions so only the auth user 
+    can add the posts
+    """
     serializer_class = PostSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-
+   
     def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(
             posts, many=True, context={'request': request}
         )
         return Response(serializer.data)
-
 
     def post(self, request):
         serializer = PostSerializer(
@@ -36,4 +42,48 @@ class PostList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class PostDetail(APIView):
+    """
+    In post detail you can search post via ID and edit or delete post. 
+    But only the auth user can edit or delets the post
+    """
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer|(
+            post, context=={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.gte_object(pk)
+        serializer = PostSerializer(
+            posts, data=request.data. context=={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.erros, status=status.HTTP_400-BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
