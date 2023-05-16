b6ac0e8156c995aa306e394c30a7855580bf8b72
AsmaaAamir <>
2023-05-16 17:45:47 +0000

    added root route,formated date and time

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,6 +2,7 @@
 # -------------------------------------------------
 # 3rd Parties:-
 from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 # internal:
 from comments.models import Comment
@@ -16,11 +17,19 @@ class CommentsSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
     
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)   
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)   
 
     class Meta:
         model = Comment
