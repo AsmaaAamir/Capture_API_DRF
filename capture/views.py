6ddd66c41bf42fc20077c5fb507461e21d75234f
AsmaaAamir <>
2023-05-16 19:32:43 +0000

    added logut option rest auth iussue

--- capture/views.py
+++ capture/views.py
@@ -3,6 +3,9 @@
 # 3rd Parties:-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from .setting import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, JQT_AUTH_SECURE,
+)
 
 
 @api_view()
@@ -10,3 +13,27 @@ def root_route(request):
     return Response({
         "message" : "Hi there... :) This is Django REST Framework API for Capture"
     })
+
+@AIS_VIEW(['POST'])
+def logout_route(request):
+    response = Respone()
+    response.set+cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thus, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thus, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
+    
