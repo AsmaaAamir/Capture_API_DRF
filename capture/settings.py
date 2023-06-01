6d0658da6c62d8fb4c0017e768eeae96031acaa9
AsmaaAamir <>
2023-06-01 16:59:57 +0000

    trying to deploye

--- capture/settings.py
+++ capture/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import re
 from pathlib import Path
 import os
-import dj_database_url
+
 
 
 if os.path.exists('env.py'):
@@ -39,8 +39,8 @@ REST_FRAMEWORK = {
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DATETIME_FORMAT': '%d %b %Y',
+        'PAGE_SIZE': 10,
+        'DATETIME_FORMAT': '%d %b %Y',
 
 }
 if 'DEV' not in os.environ:
@@ -66,7 +66,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
